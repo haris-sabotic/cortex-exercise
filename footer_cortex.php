a6197255b2927a4c03f84b502f9d8ec0a0b32435
VaskeVP <peropetar05@gmail.com>
2023-03-06 21:49:28 +0100

    Footer added

---
+++ footer_cortex.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- footer -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+        footer {
+            margin-top: 80vh;
+            padding-left: 35vh;
+            display: flex;
+            background-color: gray;
+        }
+        footer>div {
+            margin-left: 10vh;
+            margin-right: 10vh;
+            display: flex;
+            flex-direction: column;
+            text-align: center;
+        }
+        .f-icons {
+            height: 3vh; 
+            width: 3vh;
+        }
+    </style>
+</head>
+<body>
+    <footer>
+        <div>
+            <h3>Help</h3>
+            <a href="">FAQ</a>
+            <span>E-mail: <span><a href="">zapplapp@yahoo.com</a>
+        </div>
+        <div>
+            <h3>Information</h3>
+            <a href="">Contact</a>
+            <a href="">About us</a>
+            <a href="">Terms of service</a>
+            <a href="">Privacy Policy</a>
+        </div>
+        <div>
+            <h3>Advertising</h3>
+            <a href="">Pricing</a>
+            <a href="">Advices for safe use</a>
+        </div>
+        <div>
+            <a href="https://facebook.com"><img class="f-icons" src="https://cdn-icons-png.flaticon.com/512/59/59439.png" alt=""></a>
+            <a href="https://instagram.com"><img class="f-icons" src="https://icon2.cleanpng.com/20180407/cte/kisspng-computer-icons-logo-logo-instagram-5ac88fc414f009.4634305715230934440858.jpg" alt=""></a>
+            <a href="https://twitter.com"><img class="f-icons" src="https://toppng.com/uploads/preview/twitter-logo-png-transparent-background-graphic-freeuse-twitter-logo-black-11562911265gfzrhbl86r.png" alt=""></a>
+        </div>
+    </footer>
+</body>
+</html>
